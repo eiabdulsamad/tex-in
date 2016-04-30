0ce7c0924ba444933fe94d6463f36d0a8ef6a607
Abdul Samad <abdul.samad@mdrift.in>
2016-04-30 10:28:09 +0530

    updated

--- js/init.js
+++ js/init.js
@@ -161,39 +161,6 @@ function getUrlVars() {
   return vars;
 }
 
-
-
-//Language setup
-
-function placeholderLanguage() {
-  var placeholders = document.querySelectorAll('input[placeholder], textarea[placeholder]');
-  if (placeholders.length) {
-    placeholders = Array.prototype.slice.call(placeholders);
-    var div = $('<div id="placeholders" style="display:none;"></div>');
-    placeholders.forEach(function(input){
-      var text = input.placeholder;
-      div.append('<div>' + text + '</div>');    
-    });
-    $('body').append(div);
-    var originalPH = placeholders[0].placeholder;
-    setInterval(function(){
-      if (isTranslated()) {
-        updatePlaceholders();
-        originalPH = placeholders[0].placeholder;
-      }
-    }, 0);
-    function isTranslated() {
-      var currentPH = $($('#placeholders > div')[0]).text();
-      return !(originalPH == currentPH);
-    }
-    function updatePlaceholders() {
-      $('#placeholders > div').each(function(i, div){
-        placeholders[i].placeholder = $(div).text();
-      });
-    }
-  }
-}
-
 $(window).load(function() {
   var footerScroll = new Swiper('.footer-scroll', {
     nextButton: '.swiper-button-next',
@@ -202,18 +169,4 @@ $(window).load(function() {
     autoplay: 3000,
     loop: true,
   });
-});
-
-
- function languageSelector(lang) {
-    var $frame = $('.goog-te-menu-frame:first');
-    $frame.contents().find('.goog-te-menu2-item span.text:contains('+lang+')').get(0).click();
-  }
-
-
-//Language setup
-$(window).load(function() {
-  var getLang = getUrlVars()["lang"];
-  languageSelector(lang);
-  placeholderLanguage();
 });
